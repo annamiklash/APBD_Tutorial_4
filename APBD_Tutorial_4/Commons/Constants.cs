54b78ccd9e9014580b9478c3ae91841208af05b4
annamiklash <aniamiklash@gmail.com>
2020-04-11 10:35:59 +0200

    Fixed previous version. Added Middlewares and Constants

---
+++ APBD_Tutorial_4/Commons/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD_Tutorial_4
+{
+    public class Constants
+    {
+        public const string LOG_FILE = @"logs.txt";
+    }
+}
---
+++ APBD_Tutorial_4/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace APBD_Tutorial_4.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            //log errors to file
+
+            if (httpContext.Request != null)
+            {
+                string path = httpContext.Request.Path;
+                string query = httpContext.Request?.QueryString.ToString();
+                string method = httpContext.Request?.Method;
+                string bodyParameters = "";
+
+                using (StreamReader streamReader = new StreamReader(httpContext.Request.Body, Encoding.UTF8, true , 1024, true))
+                {
+                    bodyParameters = await streamReader.ReadToEndAsync();
+                }
+                
+                if (File.Exists(Constants.LOG_FILE))
+                {
+                    File.Delete(Constants.LOG_FILE);
+                }
+                FileStream writer = new FileStream(Constants.LOG_FILE, FileMode.Create);
+                writer.Close();
+
+                await using TextWriter textWriter = new StreamWriter(Constants.LOG_FILE, true);
+                textWriter.WriteLine("Path: " + path + 
+                                     "; Query: " + query + "; " +
+                                     "Method: " + method + 
+                                     ",Body Parameters: " + bodyParameters);
+            }
+            
+            await _next(httpContext);
+        }
+            
+    }
+}
